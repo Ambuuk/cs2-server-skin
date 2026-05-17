1fd81c8cad055e07a7729576529ce9ee88f353d7
Ambuuk <amarbold@mopcapital.com>
2026-05-17 15:29:01 +0800

    init

---
+++ class/config.php
@@ -0,0 +1,16 @@
+<?php
+define('SKIN_LANGUAGE', 'skins_en');
+
+define('DB_HOST', 'mysql-2b42354d-ambuuk-39ec.c.aivencloud.com');
+define('DB_PORT', '15148');
+define('DB_NAME', 'defaultdb');
+define('DB_USER', 'avnadmin');
+define('DB_PASS', 'AVNS_fYVEpEn86O2oXv5MdkU');
+
+define('WEB_STYLE_DARK', true);
+
+define('STEAM_API_KEY', 'E511B91691479382EA0D247BB8FF426E');
+define('STEAM_DOMAIN_NAME', '');
+define('STEAM_LOGOUT_PAGE', '');
+define('STEAM_LOGIN_PAGE', '');
+
